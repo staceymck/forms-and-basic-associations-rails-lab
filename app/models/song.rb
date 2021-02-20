6f423c1da5362dca62c40c8a0a4594d1434ec732
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-19 18:44:41 -0600

    commit tree 7676a335e2edbe5568c917a313eb3204250f9c23

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,20 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def notes=(notes)
+    notes.each do |note_content|
+      note = Note.find_or_create_by(content: note_content)
+      self.notes << note
+    end
+  end
 end
